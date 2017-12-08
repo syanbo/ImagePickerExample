8ad75cc8c5af1b0a365fe13f639484c3bfb319e9
Syan <hanhun@163.com>
2017-12-08 12:07:58 +0800

    更新0.0.8

--- App.js
+++ App.js
@@ -6,7 +6,6 @@
 
 import React, {Component} from 'react';
 import {
-    Platform,
     StyleSheet,
     Text,
     View,
@@ -16,9 +15,9 @@ import {
     Dimensions
 } from 'react-native';
 
-import ImagePicker from 'react-native-syan-image-picker'
+import SYImagePicker from 'react-native-syan-image-picker'
 
-const {width, height} = Dimensions.get('window');
+const {width} = Dimensions.get('window');
 
 export default class App extends Component<{}> {
 
@@ -29,15 +28,46 @@ export default class App extends Component<{}> {
         };
     }
 
-    openImagePicker = () => {
-       ImagePicker.showImagePicker({imageCount:9}, (err, photos) => {
-           console.log(err, photos);
-           if (!err) {
-               this.setState({
-                   photos: [...this.state.photos, ...photos]
-               })
-           }
-       })
+    handleOpenImagePicker = () => {
+        SYImagePicker.showImagePicker({imageCount: 9}, (err, photos) => {
+            console.log(err, photos);
+            if (!err) {
+                this.setState({
+                    photos: [...this.state.photos, ...photos]
+                })
+            }
+        })
+    };
+
+    // 以Promise形式调用
+    // 相册参数暂时只支持默认参数中罗列的属性；
+    // 使用方式
+    // sync/await
+
+    handleAsyncSelectPhoto = async () => {
+        try {
+            const photos = await SYImagePicker.asyncShowImagePicker({imageCount: 3});
+            // 选择成功
+            this.setState({
+                photos: [...this.state.photos, ...photos]
+            })
+        } catch (err) {
+            // 取消选择，err.message为"取消"
+        }
+    };
+
+    // promise形式
+    handlePromiseSelectPhoto = () => {
+        SYImagePicker.asyncShowImagePicker({imageCount: 3})
+            .then(photos => {
+                // 选择成功
+                this.setState({
+                    photos: [...this.state.photos, ...photos]
+                })
+            })
+            .catch(err => {
+                // 取消选择，err.message为"取消"
+            })
     };
 
     render() {
@@ -48,13 +78,25 @@ export default class App extends Component<{}> {
             <View style={styles.container}>
                 <TouchableOpacity
                     style={styles.btn}
-                    onPress={this.openImagePicker}
+                    onPress={this.handleOpenImagePicker}
                 >
-                    <Text style={{color:'#fff', fontSize: 16}}>选择照片</Text>
+                    <Text style={{color: '#fff', fontSize: 16}}>选择照片</Text>
                 </TouchableOpacity>
-                <ScrollView style={{flex:1}} contentContainerStyle={styles.scroll}>
+                <TouchableOpacity
+                    style={styles.btn}
+                    onPress={this.handleAsyncSelectPhoto}
+                >
+                    <Text style={{color: '#fff', fontSize: 16}}>选择照片(Async)</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.btn}
+                    onPress={this.handlePromiseSelectPhoto}
+                >
+                    <Text style={{color: '#fff', fontSize: 16}}>选择照片(Promise)</Text>
+                </TouchableOpacity>
+                <ScrollView style={{flex: 1}} contentContainerStyle={styles.scroll}>
                     {photos.map((photo, index) => {
-                        return(
+                        return (
                             <Image
                                 key={`image-${index}`}
                                 style={styles.image}
@@ -75,21 +117,21 @@ const styles = StyleSheet.create({
         backgroundColor: '#F5FCFF',
     },
     btn: {
-        backgroundColor:'#FDA549',
-        justifyContent:'center',
-        alignItems:'center',
-        height:44,
-        marginTop:20
+        backgroundColor: '#FDA549',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 44,
+        marginTop: 20
     },
     scroll: {
-        padding:10,
-        flexWrap:'wrap',
-        flexDirection:'row'
+        padding: 10,
+        flexWrap: 'wrap',
+        flexDirection: 'row'
     },
     image: {
-        margin:10,
-        width: (width - 80) /3,
+        margin: 10,
+        width: (width - 80) / 3,
         height: (width - 80) / 3,
-        backgroundColor:'#F0F0F0'
+        backgroundColor: '#F0F0F0'
     },
 });
